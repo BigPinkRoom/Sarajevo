19443954d6a1fb82380a4d72dc9fa030a0d4a566
tester2020 <tester2020@tester2020.com>
2020-09-08 22:46:43 +0300

    finished jQuery and fixed bugs of html and css

--- js/app.js
+++ js/app.js
@@ -1,7 +1,55 @@
 $(document).ready(function() {
-
-    $('#test').on('click', function() {
-        console.log('test')
+    let $header = $('#header'),
+        $intro = $('#intro');
+    
+    // toggle burger
+    $('#nav-toggle').on('click', function() {
+        $header.toggleClass('active');
     })
 
+    // hide burger on click
+    $('#header .nav__link').on('click', function() {
+        $(this).closest(".active").removeClass("active");
+    })
+
+    // fix header
+    checkScroll(); // check scroll, without scroll
+
+    $(window).on('scroll', function() {
+        checkScroll()
+    })
+
+    function checkScroll() {
+        if ( $(window).scrollTop() >= $intro.height() ) {
+            $header.addClass('fixed');
+        } else {
+            $header.removeClass('fixed');
+        }
+    }
+
+    // soft scroll
+    $('#header .nav__link').on('click', function(event) {
+        event.preventDefault()
+
+        let $id = '#' + $(this).attr('dataset-scroll');
+
+        $('body, html').animate({
+            scrollTop: $($id).offset().top,
+        }, 1000)
+        
+    })
+
+    // gallery filter
+    $('#works .gallery__button').on('click', function() {
+        let category = $(this).text();
+        category = category.split(' ').join('_');
+
+        if (category === 'All') {
+            $('#works img[data-category').show();
+            return
+        }
+
+        $('#works img[data-category').show();
+        $('#works img[data-category]').not('img[data-category='+ category + ']').hide();
+    })
 })
